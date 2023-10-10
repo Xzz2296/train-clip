0f487ac366de37165f1a0e88de4c008249876273
Xzz2296 <2014220336@qq.com>
2023-10-10 17:13:18 +0800

    10.10 update

--- models/model.py
+++ models/model.py
@@ -224,13 +224,14 @@ class ResidualAttentionBlock2(nn.Module):
         self.patch =16
         self.embed_dim = 768
         self.num_tokens = 2
-        if deep and not last_layer:
-            self.dropout = nn.Dropout(p=0.2)
-            # 每个 ResidualAttentionBlock 都会在后面的前向传播中被逐一执行，共执行 layers 次
-            val = math.sqrt(6. / float(3 * reduce(mul, [self.patch, self.patch], 1) + self.embed_dim))  # noqa
-            self.prompt_embeddings = nn.Parameter(torch.zeros(
-                self.num_tokens, self.embed_dim), requires_grad=True)
-            nn.init.uniform_(self.prompt_embeddings.data, -val, val)
+        self.last =last_layer
+        self.dropout = nn.Dropout(p=0.2)
+        # 每个 ResidualAttentionBlock 都会在后面的前向传播中被逐一执行，共执行 layers 次
+        val = math.sqrt(6. / float(3 * reduce(mul, [self.patch, self.patch], 1) + self.embed_dim))  # noqa
+        self.prompt_embeddings = nn.Parameter(torch.zeros(
+            self.num_tokens, self.embed_dim), requires_grad=True)
+        nn.init.uniform_(self.prompt_embeddings.data, -val, val)
+        # if self.Deep and not self.last:
 
         self.attn = nn.MultiheadAttention(d_model, n_head)
         self.ln_1 = LayerNorm(d_model)
@@ -250,7 +251,7 @@ class ResidualAttentionBlock2(nn.Module):
         B = x.size()[1]  # X[patch ** 2+1, Batch , width]
         # 将prompt embedding 进行了拓展，第一个维度拓展到batch大小
         expand_prompt_embeddings = self.prompt_embeddings.expand(B, -1, -1).permute(1, 0, 2)
-        if self.count > 0:
+        if self.Deep and not self.last:
             # x = torch.cat((
             #             x[:, :1, :],
             #             self.dropout(self.prompt_embeddings.expand(B, -1, -1)),
@@ -261,9 +262,9 @@ class ResidualAttentionBlock2(nn.Module):
                 x[:1, :, :],
                 self.dropout(expand_prompt_embeddings),
                 # self.dropout(self.prompt_embeddings.expand(-1, B, -1)),
-                x[1:, :, :]
+                x[1+self.num_tokens:, :, :]
             ), dim=0)
-        self.count += 1
+        # self.count += 1
         x = x + self.attention(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
